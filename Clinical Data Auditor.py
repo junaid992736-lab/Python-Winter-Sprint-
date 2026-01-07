f551d2e4dde2983421c2dcb508181085feb7a3b2
Md_Ansar <junaid992736@gmail.com>
2026-01-07 19:31:14 +0530

    Clinical Data Recording Program
    This Program Takes Patient Details , Print Data Of Patient By And Use Some String Operations

---
+++ Clinical Data Auditor.py
@@ -0,0 +1,14 @@
+#Clinical Data Recording Program
+Patient_name=input("Enter Patient Name")
+Age=input("Enter Patient Age")
+Heart_Rate=input("Enter Patient Heart Rate")
+City=input("Enter Patient City")
+
+Patient_name=Patient_name.title()
+City=City.upper()
+
+print("\n      CLINICAL DATA RECORD     ")
+print("Patient Name : ",Patient_name)
+print("Patient Age : ",Age)
+print("Patient Heart Rate (bpm) : ",Heart_Rate)
+print("Patient City : ",City)
